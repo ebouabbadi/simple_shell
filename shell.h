87cfe6a774c29eb03c1e7046414b6dbfa12080da
el mehdi bouabbadi <ebouabbadi@gmail.com>
2023-08-23 23:27:50 +0100

    push

--- shell.h
+++ shell.h
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   shell.h                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: ebouabba <ebouabba@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/22 07:29:20 by hbadr             #+#    #+#             */
-/*   Updated: 2022/10/26 17:59:06 by ebouabba         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #ifndef SHELL_H
 #define SHELL_H
 
@@ -50,50 +38,37 @@ t_global g_global;
 
 int main(int ac, char **av, char **envp);
 
-//----------> redirections <----------------//
 int redirections(t_mini *lst);
 int verification33(char c);
 void expend_inside_herdoc(int fd, char *inpt);
 void command_make(t_mini *lst, int len);
 t_list *heredoc(t_mini *lst, t_list *node);
 t_list *heredoc_handling(t_list *node, int fd);
-
-//----------> execution <----------------//
 int execution(t_mini **big_stack, t_env **envs);
 char *comd_existe(t_mini *lst);
 void check_if_cmd_builtins_and_run_intside(t_mini *lst, t_env **envs);
 void check_if_cmd_builtins_and_run_outside(t_mini *lst, t_env **envs);
 void wait_and_return_value_of_waitpid(t_mini *lst);
 char *searching_for_cmd(t_mini *lst);
-
-// --------> pipe <---------------------//
 void ft_pipe(t_mini *lst);
 void close_pipe_in_main_process(t_mini *lst);
 void pipe_control(t_mini *lst);
-
-//----------> execution util <----------------//
 char **extract_env(t_env **envs);
 int len_(t_env **envs);
 int lst_size(t_mini **stack);
 void ft_unlink(t_mini **big_stack);
 int if_cmd_in_builtins(t_mini *lst);
 int len__(t_env **envs);
-
-//----------> minishell util <----------------//
 int if_number2(char *val);
 void add_to_trash(void *ptr);
 char *my_ft_strdup(const char *s1);
 char *my_ft_substr(char const *s, unsigned int start, size_t len);
-
-//----------> env <----------------//
 void add_to_list_env(t_env *lst, t_env **env);
 void affich_env(t_env **stack);
 t_env **ft_env(char **envp);
 char *search_in_env(t_env **envs, char *variable);
 void creat_env(t_env **stack, char *name, char *val);
 int if_number(char *val);
-
-//----------> export <----------------//
 void ft_export(t_mini *lst);
 void affich_export(t_env **envs);
 void add_to_env(char *variable, char *valleur, t_env **envs);
@@ -101,11 +76,7 @@ int if_variable_exist(char *variable, char *valleur, int flag, int append);
 int if_not_a_valid_identifier(char *variable);
 char *find_value2(char *cmd);
 char *find_value1(char *cmd);
-
-//----------> unset <----------------//
 void ft_unset(t_env **envs, t_mini *lst);
-
-//----------> cd  <----------------//
 void ft_cd(t_env **envs, char **cmd, int size);
 void modif_pwd_in_env(t_env **envs);
 char *search_in_env(t_env **envs, char *variable);
@@ -115,25 +86,14 @@ void if_cd_and_argument2(char **cmd, int size, int *flag);
 void if_cd_and_minus3(int size);
 void if_cd_and_minus2(void);
 void home_not_set(void);
-//----------> pwd  <----------------//
 void ft_pwd(void);
-
-// ---------> exit <-----------------//
 void ft_exit(t_mini *lst);
-
-//---------->  echo  <----------------//
 void ft_echo(t_mini *lst);
-
-//---------->  signal <----------------//
 void fanc_aplic(int status);
 int get_c(void);
-
-//---------->  print msg <----------------//
 void msg_perror(char *str, int case_);
 void msg_error_exprt_unst(char *str, char *msg);
 void msg_error_cd(char *msg);
-
-//---------->  parsing  <----------------//
 t_mini **parsing(char *line1);
 void line_handling(char *line, t_list **stack);
 int pipe_handling(int i, t_list **stack);
