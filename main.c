92d93e6f376e7c3354a9c83df87858a52e67da99
el mehdi bouabbadi <ebouabbadi@gmail.com>
2023-08-24 02:03:47 +0100

    push

--- main.c
+++ main.c
@@ -13,34 +13,34 @@ int main(int ac, char *av[])
 
 	(void)ac;
 	signal(SIGINT, _handle_sigint);
-	envMgt(INIT_ENV, NULL, NULL);
-	feedEnvVar(__environ);
-	bMgt(SET_BUILTIN, "exit", __exit);
-	bMgt(SET_BUILTIN, "env", _env);
-	bMgt(SET_BUILTIN, "setenv", _setenv);
-	bMgt(SET_BUILTIN, "unsetenv", _unsetenv);
-	bMgt(SET_BUILTIN, "cd", _cd);
-	globalStates(SET_SHELL_NAME, &av[0]);
+	_enviroment_management(INIT_ENV, NULL, NULL);
+	_feed_enviroment_variable(__environ);
+	_builtin_management(SET_BUILTIN, "exit", __exit);
+	_builtin_management(SET_BUILTIN, "env", _env);
+	_builtin_management(SET_BUILTIN, "setenv", _setenv);
+	_builtin_management(SET_BUILTIN, "unsetenv", _unsetenv);
+	_builtin_management(SET_BUILTIN, "cd", _cd);
+	_global_states(SET_SHELL_NAME, &av[0]);
 	while (1)
 	{
-		ppt();
-		getLinee(&line);
+		_prompt();
+		_getline(&line);
 		if (!line)
 		{
 			free(line);
 			break;
 		}
-		no_comment_line = trimWhiteSpc(line);
+		no_comment_line = _trim_white_space(line);
 		free(line);
 		line = no_comment_line;
-		no_comment_line = excludeComt(line);
+		no_comment_line = _exclude_comment(line);
 		free(line);
 		line = no_comment_line;
-		globalStates(SET_LINE, &line);
-		globalStates(INCREMENT_LINE_NUMBER, NULL);
-		seminHandler(line);
+		_global_states(SET_LINE, &line);
+		_global_states(INCREMENT_LINE_NUMBER, NULL);
+		_semicolon_handler(line);
 		free(line);
 	}
-	envMgt(CLEAR_ENV, NULL, NULL);
-	return (statusMgt(GET_STATUS, 0));
+	_enviroment_management(CLEAR_ENV, NULL, NULL);
+	return (_status_management(GET_STATUS, 0));
 }
