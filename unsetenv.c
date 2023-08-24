b7df6630170d4ebd388b9b8a9b7b88b3ddeae6db
el mehdi bouabbadi <ebouabbadi@gmail.com>
2023-08-24 02:09:46 +0100

    push

--- unsetenv.c
+++ unsetenv.c
@@ -4,21 +4,21 @@
  * _unsetenv - function unset one entry from enviroment
  * variable
  *
- * @cmdxd: cmdxd that contains information about
+ * @command: command that contains information about
  * entry that will be delete such as key of the entry
  * Return: status code
  */
-int _unsetenv(cmdxd_t *cmdxd)
+int _unsetenv(command_t *command)
 {
 	int len;
 
-	len = stringArr2dlenn(cmdxd->arguments);
+	len = stringArr2dlenn(command->arguments);
 	if (len != 2)
 	{
 		myFprint(2, "%s: Invalid number of arguments\n",
-				globalistNodeates(GET_SHELL_NAME, NULL));
+				 globalistNodeates(GET_SHELL_NAME, NULL));
 		return (1);
 	}
-	envMgt(DELETE_ENTRY, cmdxd->arguments[1], NULL);
+	envMgt(DELETE_ENTRY, command->arguments[1], NULL);
 	return (0);
 }
--- utils_functions.c
+++ utils_functions.c
@@ -1,19 +1,19 @@
 #include "shell.h"
 
 /**
- *  ctEnvintoarray - function that turns
+ *  _convert_env_into_2d_array - function that turns
  * env into array 2d
  *
  * Return: array 2d or NULL signifying error
  */
-char **ctEnvintoarray(void)
+char **_convert_env_into_2d_array(void)
 {
 	list_t *keys, *iterator;
 	size_t len_iter;
 	char **array;
 	char *tmp[2];
 
-	iterator = keys = envMgt(GET_KEYS, NULL, NULL);
+	iterator = keys = _enviroment_management(GET_KEYS, NULL, NULL);
 	len_iter = _listlen(keys);
 	array = malloc(sizeof(char *) * (len_iter + 1));
 	if (!array)
@@ -25,11 +25,11 @@ char **ctEnvintoarray(void)
 	len_iter = 0;
 	while (iterator)
 	{
-		tmp[0] = _strArrcat(iterator->data, "=");
-		tmp[1] = envMgt(GET_VALUE,
+		tmp[0] = _strcat(iterator->data, "=");
+		tmp[1] = _enviroment_management(GET_VALUE,
 										iterator->data,
 										NULL);
-		array[len_iter] = _strArrcat(tmp[0], tmp[1]);
+		array[len_iter] = _strcat(tmp[0], tmp[1]);
 		free(tmp[0]);
 		free(tmp[1]);
 		iterator = iterator->next;
@@ -40,22 +40,22 @@ char **ctEnvintoarray(void)
 }
 
 /**
- * feedEnvVar - function take whatever
+ * _feed_enviroment_variable - function take whatever
  * is inside given 2d enviroment and split by space
  * and add it to the global env
  *
  * @new_env: enviroment as 2d array
  * Return: void;
  */
-void feedEnvVar(char **new_env)
+void _feed_enviroment_variable(char **new_env)
 {
 	char **entry;
 
 	while (*new_env)
 	{
-		entry = splitMy(*new_env, "=");
-		envMgt(SET_ENTRY, entry[0], entry[1]);
-		freeesplitMy(&entry);
+		entry = _split(*new_env, "=");
+		_enviroment_management(SET_ENTRY, entry[0], entry[1]);
+		_free_split(&entry);
 		new_env++;
 	}
 }
